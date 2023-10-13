2cca67a940505b85da66e3d20767f9ad6345c721
khangalll <khangal@zerotech.mn>
2023-10-13 12:55:15 +0800

    feat: print current time

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 import socket
 
 app = Flask(__name__)
@@ -8,9 +9,11 @@ def index():
     if request.method != 'GET':
         return 'Method not Allowed', 405
     try:
+        now = datetime.now()
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
+        time = now.strftime("%H:%M:%S")
+        return render_template('index.html', hostname=host_name, ip=host_ip, time=time)
     except:
         return render_template('error.html')
 
